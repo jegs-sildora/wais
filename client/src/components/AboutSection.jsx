201335b203d290b01014884148b40a421722e207
Jegrick Sildora <jegrick.sildora@chmsu.edu.ph>
2025-03-03 02:37:09 +0800

    update

--- client/src/components/AboutSection.jsx
+++ client/src/components/AboutSection.jsx
@@ -62,18 +62,21 @@ export default function AboutSection() {
           </div>
 
           <div className='flex flex-col items-center p-6 space-y-3 text-center rounded-xl bg-forest-green'>
-            <span className='inline-block p-3 bg-bright-green rounded-full'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='w-6 h-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                style={{ fill: "rgba(0, 0, 0, 1)" }}
-              >
-                <path d='M21 4H3a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1zm-1 11a3 3 0 0 0-3 3H7a3 3 0 0 0-3-3V9a3 3 0 0 0 3-3h10a3 3 0 0 0 3 3v6z'></path>
-                <path d='M12 8c-2.206 0-4 1.794-4 4s1.794 4 4 4 4-1.794 4-4-1.794-4-4-4zm0 6c-1.103 0-2-.897-2-2s.897-2 2-2 2 .897 2 2-.897 2-2 2z'></path>
-              </svg>
-            </span>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              style={{ fill: "rgba(0, 0, 0, 1)" }}
+            >
+              <circle
+                cx='15.5'
+                cy='13.5'
+                r='2.5'
+              ></circle>
+              <path d='M12 13.5c0-.815.396-1.532 1-1.988A2.47 2.47 0 0 0 11.5 11a2.5 2.5 0 1 0 0 5 2.47 2.47 0 0 0 1.5-.512 2.486 2.486 0 0 1-1-1.988z'></path>
+              <path d='M20 4H4c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2zM4 18V6h16l.002 12H4z'></path>
+            </svg>
 
             <h1 className='text-xl font-semibold uppercase text-white'>
               Spend Smarter
